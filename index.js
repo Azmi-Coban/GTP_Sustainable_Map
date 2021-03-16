fa302f21739cf2d82d1d0486440ca9577f2df30a
Azmi Coban <s4922471@bournemouth.ac.uk>
2021-03-16 22:25:36 +0200

    Adding Shapefile to my project lol - if this works I am the best non-tech student

--- index.js
+++ index.js
@@ -12,6 +12,96 @@ var countrySet = ['GB'];
 
 var map, popup, datasource, iconLayer, centerMarker, searchURL;
 var listItemTemplate = '<div class="listItem" onclick="itemSelected(\'{id}\')"><div class="listItem-title">{title}</div>{city}<br />Open until {closes}<br />{distance} miles away</div>';
+/*
+ * 
+ * 
+ * 
+ */
+
+//Add a layers for rendering the data.
+var layers = [
+    new atlas.layer.PolygonLayer(datasource, null, {
+        filter: ['any', ['==', ['geometry-type'], 'Polygon'], ['==', ['geometry-type'], 'MultiPolygon']]	//Only render Polygon or MultiPolygon in this layer.
+    }),
+    new atlas.layer.LineLayer(datasource, null, {
+        strokeColor: 'white',
+        strokeWidth: 2,
+        filter: ['any', ['==', ['geometry-type'], 'Polygon'], ['==', ['geometry-type'], 'MultiPolygon']]	//Only render Polygon or MultiPolygon in this layer.
+    }),
+    new atlas.layer.LineLayer(datasource, null, {
+        strokeColor: 'red',
+        filter: ['any', ['==', ['geometry-type'], 'LineString'], ['==', ['geometry-type'], 'MultiLineString']]	//Only render LineString or MultiLineString in this layer.
+    }),
+    new atlas.layer.BubbleLayer(datasource, null, {
+        filter: ['any', ['==', ['geometry-type'], 'Point'], ['==', ['geometry-type'], 'MultiPoint']] //Only render Point or MultiPoints in this layer.
+    })
+];
+
+map.layers.add(layers, 'labels');
+
+//Add a click event to the layers to show a popup of what the user clicked on.
+map.events.add('click', layers, featureClicked);
+            });
+        }
+
+function loadShapeFile(data/Wessex_Map.shp) {
+popup.close();
+
+shp(data / Wessex_Map.shp).then(function (data) {
+    //Load the shapefile into the data source and overwrite any existing data. 
+    datasource.setShapes(data);
+
+    //Bring the data into view on the map.
+    map.setCamera({
+        bounds: atlas.data.BoundingBox.fromData(data),
+        padding: 50
+    });
+});
+        }
+
+function featureClicked(e) {
+    //Make sure the event occurred on a shape feature.
+    if (e.shapes && e.shapes.length > 0) {
+        //By default, show the popup where the mouse event occurred.
+        var pos = e.position;
+        var offset = [0, 0];
+        var properties;
+
+        if (e.shapes[0] instanceof atlas.Shape) {
+            properties = e.shapes[0].getProperties();
+
+            //If the shape is a point feature, show the popup at the points coordinate.
+            if (e.shapes[0].getType() === 'Point') {
+                pos = e.shapes[0].getCoordinates();
+                offset = [0, -18];
+            }
+        } else {
+            properties = e.shapes[0].properties;
+
+            //If the shape is a point feature, show the popup at the points coordinate.
+            if (e.shapes[0].type === 'Point') {
+                pos = e.shapes[0].geometry.coordinates;
+                offset = [0, -18];
+            }
+        }
+
+        //Update the content and position of the popup.
+        popup.setOptions({
+            //Create a table from the properties in the feature.
+            content: atlas.PopupTemplate.applyTemplate(properties),
+            position: pos,
+            pixelOffset: offset
+        });
+
+        //Open the popup.
+        popup.open(map);
+    }
+}
+/*
+ * 
+ * 
+ * 
+ */
 
 function initialize() {
     //Initialize a map instance.
@@ -123,9 +213,7 @@ function initialize() {
                 },
                 filter: ['!', ['has', 'point_count']] //Filter out clustered points from this layer.
             });
-           /* map.markers.add(atlas.HtmlMarker(datasource, null, {
-                htmlContent: "<div><div class='pin bounce'></div><div class='pulse'></div></div>",
-            }));*/
+          
 
             map.layers.add(iconLayer);
 
