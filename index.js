5940897e4b5f6b86034ef8bb7e483174e7e48527
Azmi Coban <s4922471@bournemouth.ac.uk>
2021-03-16 22:42:24 +0200

    LTA v1.02

--- index.js
+++ index.js
@@ -192,64 +192,66 @@ function initialize() {
     map.events.add('click', layers, featureClicked);
 
 
-    function loadShapeFile(shapeFileURL) {
-        popup.close();
-
-        shp(shapeFileURL).then(function (data) {
-            //Load the shapefile into the data source and overwrite any existing data. 
-            datasource.setShapes(data);
-
-            //Bring the data into view on the map.
-            map.setCamera({
-                bounds: atlas.data.BoundingBox.fromData(data),
-                padding: 50
-            });
-        });
-    }
-
-    function featureClicked(e) {
-        //Make sure the event occurred on a shape feature.
-        if (e.shapes && e.shapes.length > 0) {
-            //By default, show the popup where the mouse event occurred.
-            var pos = e.position;
-            var offset = [0, 0];
-            var properties;
-
-            if (e.shapes[0] instanceof atlas.Shape) {
-                properties = e.shapes[0].getProperties();
-
-                //If the shape is a point feature, show the popup at the points coordinate.
-                if (e.shapes[0].getType() === 'Point') {
-                    pos = e.shapes[0].getCoordinates();
-                    offset = [0, -18];
-                }
-            } else {
-                properties = e.shapes[0].properties;
-
-                //If the shape is a point feature, show the popup at the points coordinate.
-                if (e.shapes[0].type === 'Point') {
-                    pos = e.shapes[0].geometry.coordinates;
-                    offset = [0, -18];
-                }
-            }
-
-            //Update the content and position of the popup.
-            popup.setOptions({
-                //Create a table from the properties in the feature.
-                content: atlas.PopupTemplate.applyTemplate(properties),
-                position: pos,
-                pixelOffset: offset
-            });
-
-            //Open the popup.
-            popup.open(map);
-        }
-    }
+    
 /*
  *
  *
  *
  */
+}
+
+function loadShapeFile(url) {
+    popup.close();
+
+    shp(url).then(function (data) {
+        //Load the shapefile into the data source and overwrite any existing data. 
+        datasource.setShapes(data);
+
+        //Bring the data into view on the map.
+        map.setCamera({
+            bounds: atlas.data.BoundingBox.fromData(data),
+            padding: 50
+        });
+    });
+}
+
+function featureClicked(e) {
+    //Make sure the event occurred on a shape feature.
+    if (e.shapes && e.shapes.length > 0) {
+        //By default, show the popup where the mouse event occurred.
+        var pos = e.position;
+        var offset = [0, 0];
+        var properties;
+
+        if (e.shapes[0] instanceof atlas.Shape) {
+            properties = e.shapes[0].getProperties();
+
+            //If the shape is a point feature, show the popup at the points coordinate.
+            if (e.shapes[0].getType() === 'Point') {
+                pos = e.shapes[0].getCoordinates();
+                offset = [0, -18];
+            }
+        } else {
+            properties = e.shapes[0].properties;
+
+            //If the shape is a point feature, show the popup at the points coordinate.
+            if (e.shapes[0].type === 'Point') {
+                pos = e.shapes[0].geometry.coordinates;
+                offset = [0, -18];
+            }
+        }
+
+        //Update the content and position of the popup.
+        popup.setOptions({
+            //Create a table from the properties in the feature.
+            content: atlas.PopupTemplate.applyTemplate(properties),
+            position: pos,
+            pixelOffset: offset
+        });
+
+        //Open the popup.
+        popup.open(map);
+    }
 }
 
 function loadStoreData() {
