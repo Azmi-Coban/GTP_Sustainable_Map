09ea3d8ad642aacc3bac6673878126217ce39b6d
Azmi Coban <s4922471@bournemouth.ac.uk>
2021-03-24 13:18:25 +0200

    size 0.5

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ function initialize() {
                     image: 'myCustomIcon',
 
                     //Optionally scale the size of the icon.
-                    size: 2,
+                    size: 0.5,
                     font: ['SegoeUi-Bold'],
 
                     //Anchor the center of the icon image to the coordinate.
