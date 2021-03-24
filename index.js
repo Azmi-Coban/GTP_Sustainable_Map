5ba4213d0671256c4fd42677a77317d8e84cb082
Azmi Coban <s4922471@bournemouth.ac.uk>
2021-03-24 12:48:28 +0200

    TreeIcon for the pins

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var maxClusterZoomLevel = 5;
 var storeLocationDataUrl = 'data/Wessex_Company_Dataset.txt';
 
 //The URL to the icon image. 
-var iconImageUrl = 'images/CoffeeIcon.png';
+var iconImageUrl = 'images/TreeIcon.png';
 
 //An array of country region ISO2 values to limit searches to.
 var countrySet = ['GB'];
